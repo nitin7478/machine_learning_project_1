9684041d904d0ab82e6050c1b1b2006cc7a9796c
nitin7478 <nitin7478@gmail.com>
2022-12-07 17:30:19 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from typing import List
+
+
+
+# declaring variables for setup function
+PROJECT_NAME = "housung-predictor"
+VERSION = "0.0.1"
+AUTHOR="Nitin Udmale"
+DESCRIPTION="This is my first machine learning project"
+PACKAGES = ["housing"]
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description : This fucntion is going to return list of requirement mentioned in
+    requirements.txt file 
+    
+    Return :  This function is going to return a list which contain name
+    of libraries mentioned is requirements.txt file 
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+install_requires = get_requirements_list()
+)
+
